63f3e29b383942ebd91eb310c12377205c643f69
look4yasharth <look4yasharth@gmail.com>
2020-03-31 07:25:31 +0530

    Watch Connector

---
+++ Project12 WatchKit Extension/ComplicationController.swift
@@ -0,0 +1,24 @@
+import ClockKit
+
+class ComplicationController: NSObject, CLKComplicationDataSource {
+    func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
+        handler([])
+    }
+
+    func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+        let template = CLKComplicationTemplateModularSmallSimpleText()
+
+        let currentText = UserDefaults.standard.string(forKey: "complication_number") ?? "?"
+        template.textProvider = CLKSimpleTextProvider(text: currentText)
+
+        let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+        handler(entry)
+    }
+
+    func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
+        // This method will be called once per supported complication, and the results will be cached
+        let template = CLKComplicationTemplateModularSmallSimpleText()
+        template.textProvider = CLKSimpleTextProvider(text: "?")
+        handler(template)
+    }
+}
